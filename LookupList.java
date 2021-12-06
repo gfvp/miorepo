65d541ec382487ec4b097beb5c6d01aeccbe9364
Giuseppe Federico <giuseppe.federico@netserv.it>
2021-12-06 09:50:52 +0100

    test commit

--- LookupList.java
+++ LookupList.java
@@ -10,7 +10,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  *
- * @author alessandro
+ * @author ale
  * @version $Id: $
  */
 public class LookupList extends ArrayList<LookupEntry>{
